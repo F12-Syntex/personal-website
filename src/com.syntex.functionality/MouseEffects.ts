c9aa944559c5c7c31dcd540f296dc51bf059b41e
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-07-18 10:48:20 +0100

    v0.1.10
    Fixed issue: Avoid use of == and !=
    JS-0050

--- src/com.syntex.functionality/MouseEffects.ts
+++ src/com.syntex.functionality/MouseEffects.ts
@@ -9,12 +9,12 @@ class MouseEffects {
     const element = document.getElementById('cursor')
     
 
-    if (element == null) return
+    if (element === null) return
 
     //make sure the custom cursor updates to the mouse position
     document.addEventListener('mousemove', function (event) {
       var cursorCircle = document.getElementById('cursor')
-      if (cursorCircle == null) return
+      if (cursorCircle === null) return
       cursorCircle.style.left = event.clientX - 10 + 'px'
       cursorCircle.style.top = event.clientY - 10 + 'px'
     })
@@ -28,7 +28,7 @@ class MouseEffects {
      * this function is responsible for handling mouse move event
      */
     function handleMouseMove() {
-      if (element == null) return
+      if (element === null) return
 
       if (element.classList.contains('mousedown')) return
 
@@ -40,7 +40,7 @@ class MouseEffects {
      * this function is responsible for handling mouse down event
      */
     function handleMouseDown() {
-      if (element == null) return
+      if (element === null) return
       element.classList.add('mousedown') // Apply 'clicking' class when mouse button is pressed
       element.classList.remove('moving', 'mouseup') // Remove other classes
     }
@@ -49,7 +49,7 @@ class MouseEffects {
      * this function is responsible for handling mouse up event
      */
     function handleMouseUp() {
-      if (element == null) return
+      if (element === null) return
       element.classList.add('mouseup') // Apply 'pressed' class when mouse button is released
       element.classList.remove('moving', 'mousedown') // Remove other classes
     }

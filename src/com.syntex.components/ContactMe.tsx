e0ec58499d1858118e8d76a1e39bc222b22a1e42
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-07-20 23:07:10 +0100

    v0.1.13
    implemented change that will make contact me icons turn white when hovered - incomplete

--- src/com.syntex.components/ContactMe.tsx
+++ src/com.syntex.components/ContactMe.tsx
@@ -1,6 +1,8 @@
 import '../com.syntex.sytling/ContactMe.css'
 import '../com.syntex.functionality/MouseEffects'
 import MouseEffects from '../com.syntex.functionality/MouseEffects'
+import { SourceTextModule } from 'vm'
+import { MouseEventHandler } from 'react'
 
 /**
  * The contact section of the website
@@ -11,12 +13,20 @@ function ContactMe() {
     window.open('https://github.com/F12-Syntex')
   }
 
-  function itemHover() {
+  function itemHover(id: string) {
     MouseEffects.setMouseHover(true)
+
+    const element = document.getElementById(id)
+    if (element === null) return
+    element.style.fill = 'white'
   }
 
-  function itemExit() {
+  function itemExit(id: string) {
     MouseEffects.setMouseHover(false)
+
+    const element = document.getElementById(id)
+    if (element === null) return
+    element.style.fill = 'gray'
   }
 
   return (
@@ -28,12 +38,11 @@ function ContactMe() {
           xmlns="http://www.w3.org/2000/svg"
           width="min(10vw, 30px)"
           height="min(5vh, 30px)"
-          fill="gray"
           viewBox="0 0 16 16"
           onClick={onGitHubClick}
-          onMouseEnter={itemHover}
-          onMouseLeave={itemExit}
-          className="contact-me-github"
+          onMouseEnter={() => itemHover("contact-me-button-github")}
+          onMouseLeave={() => itemExit("contact-me-button-github")}
+          id="contact-me-button-github"
         >
           <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
         </svg>
